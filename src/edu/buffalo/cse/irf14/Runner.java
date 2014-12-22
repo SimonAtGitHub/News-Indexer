f8175ca9220ce0d404cccd87c3edfcb665314330
Harish Varadarajan <hrsh.420@gmail.com>
2014-12-21 19:09:37 -0500

    Update Runner.java

--- src/edu/buffalo/cse/irf14/Runner.java
+++ src/edu/buffalo/cse/irf14/Runner.java
@@ -7,39 +7,23 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.PrintStream;
 import java.util.ArrayList;
-//import java.util.Date;
-
-
 
 import edu.buffalo.cse.irf14.document.Document;
-//import edu.buffalo.cse.irf14.document.FieldNames;
 import edu.buffalo.cse.irf14.document.Parser;
 import edu.buffalo.cse.irf14.document.ParserException;
 import edu.buffalo.cse.irf14.index.IndexWriter;
 import edu.buffalo.cse.irf14.index.IndexerException;
 
-/**
- * @author nikhillo
- *
- */
 public class Runner {
 
-	/**
-	 * 
-	 */
 	public Runner() {
 		// TODO Auto-generated constructor stub
 	}
 
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
-//		System.out.println("Start Time: " + new Date().toString());
 		String ipDir = args[0];
 		String indexDir = args[1];
-		//more? idk!
-		
+
 		File ipDirectory = new File(ipDir);
 		String[] catDirectories = ipDirectory.list();
 		
@@ -48,9 +32,7 @@ public class Runner {
 		
 		Document d = null;
 		IndexWriter writer = new IndexWriter(indexDir);
-		//Below is Temp collection. Delete for full implementation
-		ArrayList <Document> docCol = new ArrayList<Document>();
-		
+
 		try {
 			for (String cat : catDirectories) {
 				dir = new File(ipDir+ File.separator+ cat);
@@ -61,12 +43,8 @@ public class Runner {
 				
 				for (String f : files) {
 					try {
-                                                //System.out.println("File being Parsed: \n" + "cat : " + cat + "-" +f);
 						d = Parser.parse(dir.getAbsolutePath() + File.separator +f);
 						writer.addDocument(d);
-						//Below is Temp collection. Delete for full implementation
-						docCol.add(d);
-						
 					} catch (ParserException e) {
 						// TODO Auto-generated catch block
 						e.printStackTrace();
@@ -74,14 +52,10 @@ public class Runner {
 				}
 				
 			}
-			//System.out.println("Final collection size: " + docCol.size());
-			//System.out.println("Random Print: \nFile Name: " + docCol.get(0000005).getField(FieldNames.FILEID)[0] + " " + docCol.get(11400).getField(FieldNames.CATEGORY)[0] + "\n Content:\n" + docCol.get(11400).getField(FieldNames.CONTENT)[0] );
 			writer.close();
 		} catch (IndexerException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-//		System.out.println("End Time: " + new Date().toString());
 	}
 
 }
